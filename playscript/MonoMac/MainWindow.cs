8a3d8bc34c95a1b9baf83396f466d3fc7b422b94
Icer Addis <iaddis@zynga.com>
2013-04-13 21:34:04 -0700

    Replaced MonoMac application code with NeHeLesson code obtained from MonoMac samples. Moved all flash related code into Player.cs

--- playscript/MonoMac/MainWindow.cs
+++ playscript/MonoMac/MainWindow.cs
@@ -1,44 +1,18 @@
-
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using MonoMac.Foundation;
 using MonoMac.AppKit;
 
-namespace OpenGLLayer
+namespace PlayScriptApp
 {
-	public partial class MainWindow : MonoMac.AppKit.NSWindow
-	{
-		#region Constructors
+        public partial class MainWindow : MonoMac.AppKit.NSWindow
+        {
+                public MainWindow (IntPtr handle) : base(handle)
+                {
+                }
 
-		// Called when created from unmanaged code
-		public MainWindow (IntPtr handle) : base(handle)
-		{
-			Initialize ();
-		}
-
-		// Called when created directly from a XIB file
-		[Export("initWithCoder:")]
-		public MainWindow (NSCoder coder) : base(coder)
-		{
-			Initialize ();
-		}
-
-		// Shared initialization code
-		void Initialize ()
-		{
-			this.AcceptsMouseMovedEvents = true;
-		}
-
-		public override void MouseMoved (NSEvent theEvent)
-		{
-			// big hack here using the static, need to fix the monomac app at some point..
-			if (MyView.movingLayer != null)	MyView.movingLayer.MouseMoved(theEvent);
-		}
-
-
-		
-		#endregion
-	}
+                public MainWindow (NSCoder coder) : base(coder)
+                {
+                }
+        }
 }
 
