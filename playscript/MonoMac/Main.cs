d850de6d204d366eac332d40a478937fabc3a487
Icer Addis <iaddis@zynga.com>
2013-05-07 13:43:20 -0700

    Moved all common application code to playscript/PlayScript.Application.*, please clone the playscript repository to run the Away3D demos

--- playscript/MonoMac/Main.cs
+++ playscript/MonoMac/Main.cs
@@ -1,18 +1,21 @@
 using System;
-using System.Drawing;
-using MonoMac.Foundation;
-using MonoMac.AppKit;
-using MonoMac.ObjCRuntime;
+using PlayScript.Application;
 
-namespace PlayScriptApp
+namespace PlayScriptAway3D
 {
-        class MainClass
-        {
-                static void Main (string[] args)
-                {
-                        NSApplication.Init ();
-                        NSApplication.Main (args);
-                }
-        }
+	public class MainClass
+	{
+		// This is the main entry point of the application.
+		static void Main (string[] args)
+		{
+			//			Application.Run(args, typeof(_root.Basic_View));
+			//			Application.Run(args, typeof(_root.Basic_SkyBox));
+			//			Application.Run(args, typeof(_root.Basic_Particles));
+			//			Application.Run(args, typeof(_root.Intermediate_ParticleExplosions));
+			//			Application.Run(args, typeof(_root.Basic_Shading));
+			//			Application.Run(args, typeof(_root.Basic_Load3DS));
+			//			Application.Run(args, typeof(_root.Basic_Fire));
+			Application.Run(args, typeof(_root.Intermediate_Globe));
+		}
+	}
 }
-
--- playscript/MonoTouch/Main.cs
+++ playscript/MonoTouch/Main.cs
@@ -1,20 +1,21 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using PlayScript.Application;
 
-using MonoTouch.Foundation;
-using MonoTouch.UIKit;
-
-namespace Away3DApp
+namespace PlayScriptAway3D
 {
-	public class Application
+	public class MainClass
 	{
 		// This is the main entry point of the application.
 		static void Main (string[] args)
 		{
-			// if you want to use a different Application Delegate class from "AppDelegate"
-			// you can specify it here.
-			UIApplication.Main (args, null, "AppDelegate");
+			//			Application.Run(args, typeof(_root.Basic_View));
+						Application.Run(args, typeof(_root.Basic_SkyBox));
+			//			Application.Run(args, typeof(_root.Basic_Particles));
+			//			Application.Run(args, typeof(_root.Intermediate_ParticleExplosions));
+			//			Application.Run(args, typeof(_root.Basic_Shading));
+			//			Application.Run(args, typeof(_root.Basic_Load3DS));
+			//			Application.Run(args, typeof(_root.Basic_Fire));
+//			Application.Run(args, typeof(_root.Intermediate_Globe));
 		}
 	}
 }
