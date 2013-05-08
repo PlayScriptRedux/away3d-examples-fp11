4b6063f0652c6d239a94fa981ea2e46d56dc2a4f
Icer Addis <iaddis@zynga.com>
2013-05-07 17:14:58 -0700

    Fixes for renamed playscript-base and playscript.Application classes

--- playscript/MonoMac/Main.cs
+++ playscript/MonoMac/Main.cs
@@ -1,5 +1,5 @@
 using System;
-using PlayScript.Application;
+using playscript;
 
 namespace PlayScriptAway3D
 {
@@ -8,14 +8,14 @@ namespace PlayScriptAway3D
 		// This is the main entry point of the application.
 		static void Main (string[] args)
 		{
-			//			Application.Run(args, typeof(_root.Basic_View));
-			//			Application.Run(args, typeof(_root.Basic_SkyBox));
-			//			Application.Run(args, typeof(_root.Basic_Particles));
-			//			Application.Run(args, typeof(_root.Intermediate_ParticleExplosions));
-			//			Application.Run(args, typeof(_root.Basic_Shading));
-			//			Application.Run(args, typeof(_root.Basic_Load3DS));
-			//			Application.Run(args, typeof(_root.Basic_Fire));
-			Application.Run(args, typeof(_root.Intermediate_Globe));
+			//			Application.run(args, typeof(_root.Basic_View));
+			//			Application.run(args, typeof(_root.Basic_SkyBox));
+			//			Application.run(args, typeof(_root.Basic_Particles));
+			//			Application.run(args, typeof(_root.Intermediate_ParticleExplosions));
+			//			Application.run(args, typeof(_root.Basic_Shading));
+			//			Application.run(args, typeof(_root.Basic_Load3DS));
+			//			Application.run(args, typeof(_root.Basic_Fire));
+			Application.run(args, typeof(_root.Intermediate_Globe));
 		}
 	}
 }
