baa69a229e04dee83385f7b58ad01fad55d5dea8
SushiHangover <sushihangover@outlook.com>
2015-08-16 14:32:44 -0700

    Sync examples with playscript 3.4.1003

--- playscript/MonoMac/Main.cs
+++ playscript/MonoMac/Main.cs
@@ -7,15 +7,15 @@ namespace PlayScriptAway3D
 		// This is the main entry point of the application.
 		static void Main (string[] args)
 		{
-						//Application.run(args, typeof(_root.Basic_View));
-						//Application.run(args, typeof(_root.Basic_SkyBox));
-						//Application.run(args, typeof(_root.Basic_Particles));
-						Application.run(args, typeof(_root.Basic_Shading));
-						//Application.run(args, typeof(_root.Basic_Load3DS));
-						//Application.run(args, typeof(_root.Basic_Fire));
+			//Application.run(args, typeof(_root.Basic_View));
+			//Application.run(args, typeof(_root.Basic_SkyBox));
+			//Application.run(args, typeof(_root.Basic_Particles));
+			//Application.run(args, typeof(_root.Basic_Shading));
+			//Application.run(args, typeof(_root.Basic_Load3DS));
+			//Application.run(args, typeof(_root.Basic_Fire));
 			//Application.run(args, typeof(_root.Intermediate_Globe));
 			//Application.run(args, typeof(_root.Intermediate_ParticleExplosions));
-			//Application.run (args, typeof(_root.Intermediate_PlayScriptParticleExplosions));
+			Application.run (args, typeof(_root.Intermediate_PlayScriptParticleExplosions));
 		}
 	}
 }
